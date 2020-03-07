9876174702a9c4424849eda33370fae70e8194d9
Aleksei.Grachev <aleksei_grachev@epam.com>
2020-03-07 23:39:13 +0300

    renamed docker container name

--- db_start.sh
+++ db_start.sh
@@ -1,1 +1,1 @@
-docker run --name db_template_mysql -d -e MYSQL_ROOT_PASSWORD=root -e MYSQL_ROOT_HOST=% -p 3306:3306 -v $PWD/db:/var/lib/mysql -it mysql:8
+docker run --name finance_mysql -d -e MYSQL_ROOT_PASSWORD=root -e MYSQL_ROOT_HOST=% -p 3306:3306 -v $PWD/db:/var/lib/mysql -it mysql:8
